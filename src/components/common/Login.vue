de17c3b75fb74ae0bab4fdb4b0808df9bda95eca
Tentative <micoli.giacomo@gmail.com>
2020-03-20 20:35:14 +0100

    log in style fix

--- src/components/common/Login.vue
+++ src/components/common/Login.vue
@@ -4,7 +4,7 @@
       <md-content class="md-elevation-3">
         <div class="title">
           <img src="@/assets/img/logo.png" alt="logo" />
-          <div class="md-body-1">You are not logged in</div>
+          <div class="md-body-1 signup">Accedi</div>
         </div>
 
         <div class="form">
