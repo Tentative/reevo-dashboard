81b25acfa0fb69ecd2c1600ca76753b13ab89026
Tentative <micoli.giacomo@gmail.com>
2020-03-25 15:54:56 +0100

    filters display fix

--- src/components/AMZGraph/Alerts.vue
+++ src/components/AMZGraph/Alerts.vue
@@ -1,44 +1,65 @@
 <template>
   <div class="md-alignment-center-center settings">
-    <md-dialog :md-active.sync="showDialog">
+    <md-dialog class="filter" :md-active.sync="showDialog">
       <md-dialog-title>Filtri</md-dialog-title>
-
-      <md-tabs md-dynamic-height>
-        <md-tab>
-          <div>
-            <md-switch
-              v-model="amz.FiltroAlert"
-              true-value="Si"
-              false-value="Tutti"
-              >Filtro Alert</md-switch
-            >
-            <md-switch
-              v-model="amz.FiltroInStock"
-              true-value="No"
-              false-value="Tutti"
-              >Filtro In Stock</md-switch
-            >
-            <md-switch
-              v-model="amz.FiltroFastTrack"
-              true-value="No"
-              false-value="Tutti"
-              >Filtro Fast Track</md-switch
-            >
-            <md-switch
-              v-model="amz.FiltroBuyBox"
-              true-value="No"
-              false-value="Tutti"
-              >Filtro Buy Box</md-switch
-            >
-            <md-switch
-              v-model="amz.FiltroNegativeReviews"
-              true-value="SoloNegative"
-              false-value="Tutti"
-              >Filtro Negative Reviews</md-switch
-            >
-          </div>
-        </md-tab></md-tabs
-      >
+      <div class="md-layout">
+        <div class="md-layout-item ">
+          <md-tabs md-dynamic-height>
+            <md-tab>
+              <md-switch
+                v-model="amz.FiltroAlert"
+                true-value="Si"
+                false-value="Tutti"
+                >{{
+                  amz.FiltroAlert == "Tutti"
+                    ? "Mostra tutti gli articoli"
+                    : "Mostra solo articoli con Alert"
+                }}</md-switch
+              >
+              <md-switch
+                v-model="amz.FiltroInStock"
+                true-value="No"
+                false-value="Tutti"
+                >{{
+                  amz.FiltroInStock == "Tutti"
+                    ? "Mostra tutti gli articoli"
+                    : "Mostra solo articoli Fuori Stock"
+                }}</md-switch
+              >
+              <md-switch
+                v-model="amz.FiltroFastTrack"
+                true-value="No"
+                false-value="Tutti"
+                >{{
+                  amz.FiltroFastTrack == "Tutti"
+                    ? "Mostra tutti gli articoli"
+                    : "Mostra solo articoli non Fast Track"
+                }}</md-switch
+              >
+              <md-switch
+                v-model="amz.FiltroBuyBox"
+                true-value="No"
+                false-value="Tutti"
+                >{{
+                  amz.FiltroBuyBox == "Tutti"
+                    ? "Mostra tutti gli articoli"
+                    : "Mostra solo articoli senza BuyBox"
+                }}</md-switch
+              >
+              <md-switch
+                v-model="amz.FiltroNegativeReviews"
+                true-value="SoloNegative"
+                false-value="Tutti"
+                >{{
+                  amz.FiltroNegativeReviews == "Tutti"
+                    ? "Mostra tutti gli articoli"
+                    : "Mostra solo articoli con recensioni negative maggiori del 20%"
+                }}</md-switch
+              >
+            </md-tab></md-tabs
+          >
+        </div>
+      </div>
 
       <md-dialog-actions>
         <md-button class="md-primary" @click="showDialog = false"
@@ -68,13 +89,21 @@ export default {
       FiltroFastTrack: "Tutti",
       FiltroBuyBox: "Tutti",
       FiltroNegativeReviews: "Tutti"
-    }
+    },
+    computedAlert: ""
   }),
   methods: {
     saveDialog() {
       let amz = this.amz;
       this.showDialog = false;
       this.$store.dispatch("amz_request", { amz });
+    },
+    computedText() {
+      if (this.FiltroAlert == "Tutti") {
+        this.computedAlert = "Mostra tutti gli articoli";
+      } else {
+        this.computedAlert = "Solo articoli con alert";
+      }
     }
   }
 };
