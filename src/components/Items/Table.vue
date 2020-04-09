8f5167251dacef3bdacd38868c566bd339524dd3
Tentative <micoli.giacomo@gmail.com>
2020-04-09 18:48:03 +0200

    items table init

---
+++ src/components/Items/Table.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <md-table class="items-dashboard">
+      <md-table-row>
+        <md-table-head></md-table-head>
+        <md-table-head class="name">Articolo</md-table-head>
+        <md-table-head class="retailer"></md-table-head>
+        <md-table-head class="cost"></md-table-head>
+        <md-table-head class="cheap fix"></md-table-head>
+        <md-table-head class="oos"></md-table-head>
+        <md-table-head class="name">Articolo</md-table-head>
+        <md-table-head class="retailer"></md-table-head>
+        <md-table-head class="cost"></md-table-head>
+        <md-table-head class="cheap fix"></md-table-head>
+        <md-table-head class="oos"></md-table-head>
+        <md-table-head class="name">Articolo</md-table-head>
+        <md-table-head class="retailer"></md-table-head>
+        <md-table-head class="cost"></md-table-head>
+        <md-table-head class="cheap fix"></md-table-head>
+        <md-table-head class="oos"></md-table-head>
+        <md-table-head class="name">Articolo</md-table-head>
+        <md-table-head class="retailer"></md-table-head>
+        <md-table-head class="cost"></md-table-head>
+        <md-table-head class="cheap fix"></md-table-head>
+        <md-table-head class="oos"></md-table-head>
+      </md-table-row>
+      <md-table-row>
+        <md-table-cell></md-table-cell>
+      </md-table-row>
+    </md-table>
+  </div>
+</template>
---
+++ src/views/Items.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="md-alignment-center-center">
+    <Table />
+    <Pagination />
+    <Jumper />
+  </div>
+</template>
+
+<script>
+import { Table } from "@/components/Items/Table.vue";
+export default {
+  name: "Items",
+  components: { Table },
+  data() {
+    return {};
+  },
+};
+</script>
