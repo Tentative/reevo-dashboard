ac7cdea515d2bd9560728d5084f228fa8cb33166
Tentative <micoli.giacomo@gmail.com>
2020-05-14 08:45:29 +0200

    table format fix

--- src/components/PriceGraph/Table.vue
+++ src/components/PriceGraph/Table.vue
@@ -17,13 +17,51 @@
         class="price-table"
       >
         <md-table-cell>{{ riga.NomeRiga }}</md-table-cell>
-        <md-table-cell>{{ riga.NumeroItems }}</md-table-cell>
-        <md-table-cell>€ {{ riga.PrezzoMedioAttuale }}</md-table-cell>
-        <md-table-cell>€ {{ riga.PrezzoMedioPeriodo }}</md-table-cell>
-        <md-table-cell>€ {{ riga.PrezziGiu }}</md-table-cell>
-        <md-table-cell>€ {{ riga.PrezziSu }}</md-table-cell>
-        <md-table-cell>€ {{ riga.PrezziUguali }}</md-table-cell>
-        <md-table-cell>% {{ riga.FuoriStock }}</md-table-cell>
+        <md-table-cell
+          ><span v-if="riga.NumeroItems != 0">{{ riga.NumeroItems }}</span
+          ><span v-else class="unavailable"
+            ><md-icon>remove</md-icon></span
+          ></md-table-cell
+        >
+        <md-table-cell
+          ><span v-if="riga.PrezzoMedioAttuale != null">{{
+            Math.abs(riga.PrezzoMedioAttuale).toString().slice(0, 4)
+          }}</span>
+          <span v-else class="unavailable"><md-icon>remove</md-icon></span>
+          €</md-table-cell
+        >
+        <md-table-cell
+          ><span v-if="riga.PrezzoMedioPeriodo != null">{{
+            Math.abs(riga.PrezzoMedioPeriodo).toString().slice(0, 4)
+          }}</span>
+          <span v-else class="unavailable"><md-icon>remove</md-icon></span>
+          €</md-table-cell
+        >
+        <md-table-cell
+          ><span v-if="riga.PrezziGiu != 0">{{ riga.PrezziGiu }} €</span
+          ><span v-else class="unavailable"
+            ><md-icon>remove</md-icon></span
+          ></md-table-cell
+        >
+        <md-table-cell
+          ><span v-if="riga.PrezziSu != 0">{{ riga.PrezziSu }} €</span
+          ><span v-else class="unavailable"
+            ><md-icon>remove</md-icon></span
+          ></md-table-cell
+        >
+        <md-table-cell
+          ><span v-if="riga.PrezziUguali != 0">{{ riga.PrezziUguali }} €</span
+          ><span v-else class="unavailable"
+            ><md-icon>remove</md-icon></span
+          ></md-table-cell
+        >
+        <md-table-cell
+          ><span v-if="riga.FuoriStock != null"
+            >{{ Math.abs(riga.FuoriStock).toString().slice(0, 2) }} % </span
+          ><span v-else class="unavailable"
+            ><md-icon>remove</md-icon></span
+          ></md-table-cell
+        >
       </md-table-row>
     </md-table>
   </div>
