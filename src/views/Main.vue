9490d26e36770f7ad9a6f1da28920c752abfa062
Tentative <micoli.giacomo@gmail.com>
2020-05-20 14:34:44 +0200

    style fix

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -5,6 +5,14 @@
         Main
       </div>
     </div>
+    <div class="sub-wrapper md-layout md-gutter">
+      <div class="md-layout-item">
+        <span>Principali Alert di Prezzo</span>
+      </div>
+      <div class="md-layout-item">
+        <span>Principali Alert Fuori Stock</span>
+      </div>
+    </div>
     <md-dialog
       :md-active.sync="loading"
       class="graph md-scrollbar loading"
