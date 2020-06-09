2609400f6e83868d79e08f73690840ed0c1b77f2
Tentative <micoli.giacomo@gmail.com>
2020-06-09 23:54:49 +0200

    first try (partial)

--- src/store/modules/amzgraph.js
+++ src/store/modules/amzgraph.js
@@ -1,7 +1,6 @@
 import axios from "axios";
-import moment from "moment";
 import global from "@/store/modules/global.js";
-
+import moment from "moment";
 export default {
   state: {
     amzGraphVisible: false,
@@ -9,193 +8,6 @@ export default {
     graph_res: {},
     graph_days: [],
     currentItem: {},
-
-    chartdata: {
-      labels: [],
-      datasets: [
-        {
-          label: "Prices",
-          type: "line",
-          yAxisID: "prices",
-        },
-        {
-          label: "Sales Rank",
-          type: "line",
-          yAxisID: "ranks",
-        },
-        {
-          label: "Out of stock",
-
-          type: "bar",
-
-          yAxisID: "stock",
-        },
-      ],
-    },
-    options: {
-      legend: {
-        labels: {
-          useLineStyle: true,
-          // eslint-disable-next-line no-unused-vars
-          filter: function (item, chart) {
-            // Logic to remove a particular legend item goes here
-            return !item.text.includes("Out of stock");
-          },
-        },
-      },
-      annotation: {
-        annotations: [
-          {
-            id: "min-line",
-            mode: "horizontal",
-            type: "line",
-            scaleID: "prices",
-            value: "",
-            borderColor: "rgba(255,0,0,0.4)",
-            borderWidth: 2,
-            borderDash: [2, 2],
-          },
-          {
-            id: "max-line",
-            mode: "horizontal",
-            type: "line",
-            scaleID: "prices",
-            value: "",
-            borderColor: "rgba(0,204,102,0.4)",
-            borderWidth: 2,
-            borderDash: [2, 2],
-          },
-        ],
-        drawTime: "afterDraw",
-      },
-      responsive: true,
-      maintainAspectRatio: false,
-      showLines: true,
-      scales: {
-        yAxes: [
-          {
-            id: "prices",
-            type: "linear",
-            position: "left",
-            gridLines: {
-              display: true,
-              offsetGridLines: false,
-            },
-            ticks: {
-              display: true,
-              // eslint-disable-next-line no-unused-vars
-              callback: function (value, index, values) {
-                return value + " " + "€";
-              },
-              beginAtZero: false,
-              // suggestedMax: "",
-              bounds: "data",
-              // min: "",
-              // max: "",
-              precision: 0,
-            },
-            afterTickToLabelConversion: function (scaleInstance) {
-              // set the first and last tick to null so it does not display
-              // note, ticks[0] is the last tick and ticks[length - 1] is the first
-              scaleInstance.ticks[scaleInstance.ticks.length - 1] = null;
-
-              // need to do the same thing for this similiar array which is used internally
-              scaleInstance.ticksAsNumbers[
-                scaleInstance.ticksAsNumbers.length - 1
-              ] = null;
-            },
-          },
-          {
-            id: "ranks",
-            type: "linear",
-            position: "right",
-            gridLines: {
-              display: false,
-              drawBorder: true,
-              lineWidth: 1,
-              zeroLineWidth: 1,
-              offsetGridLines: false,
-            },
-            ticks: {
-              display: false,
-              reverse: true,
-              precision: 0,
-              bounds: "data",
-            },
-          },
-          {
-            id: "stock",
-            distribution: "linear",
-            gridLines: {
-              display: false,
-            },
-            ticks: {
-              display: false,
-              precision: 0,
-              source: "data",
-              // min: "",
-              // max: ""
-            },
-          },
-        ],
-        xAxes: [
-          {
-            barPercentage: 1.1,
-            categoryPercentage: 1.1,
-
-            // bounds: "ticks",
-            // type: "time",
-            ticks: {
-              // min: "",
-              // max: "",
-              // source: "ticks",
-              userCallback: function (item, index) {
-                if (!(index % 3)) return moment(item).format("DD MMM");
-              },
-              autoSkip: true,
-              maxRotation: 0,
-              maxTicksLimit: 30,
-            },
-            distribution: "linear",
-            time: {
-              displayFormats: {
-                day: "D MMM",
-              },
-              // stepSize: 3,
-              unit: "day",
-            },
-            gridLines: {
-              display: true,
-              drawTicks: false,
-              offsetGridLines: false,
-              zeroLineWidth: 1,
-            },
-            offset: false,
-          },
-        ],
-      },
-      tooltips: {
-        mode: "index",
-        position: "nearest",
-        callbacks: {
-          title: (tooltipItems) => {
-            return tooltipItems[0].xLabel;
-          },
-
-          //
-          // eslint-disable-next-line no-unused-vars
-          label: function (t, d) {
-            if (t.datasetIndex === 0) {
-              return "Prezzo: " + t.yLabel + " " + "€";
-            } else if (t.datasetIndex === 1) {
-              return "Sales Rank: " + t.yLabel;
-            }
-          },
-        },
-      },
-    },
-    days: [],
-    min_max: [],
     amzgraph_request: {
       CodiceClient: "reevolacerba2020",
       CodiceRichiesta: "AMZGraph",
@@ -203,8 +15,59 @@ export default {
       Url: window.location.href,
       //define JsonRichiesta from mutations;
     },
-  },
 
+    chartdata: {
+      labels: [],
+      datasets: [],
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      legend: {
+        display: true,
+        labels: {
+          useLineStyle: true,
+        },
+        position: "bottom",
+      },
+      scales: {
+        yAxes: [],
+        xAxes: [
+          {
+            offset: false,
+            bounds: "data",
+            // distribution: "linear",
+            type: "time",
+            autoSkip: false,
+            time: {
+              displayFormat: {
+                day: "D MMM",
+              },
+              unit: "day",
+              unitStepSize: 3,
+              // stepSize: 3,
+            },
+            ticks: {
+              source: "data",
+              padding: 15,
+              autoSkip: false,
+              maxRotation: 0,
+              callback: function (item, index) {
+                if (!(index % 3)) return item;
+              },
+            },
+            gridLines: {
+              drawTicks: false,
+              drawBorder: true,
+            },
+          },
+        ],
+      },
+    },
+    prcdata: {},
+    retailers: {},
+    listaRigheTabella: {},
+  },
   mutations: {
     toggle_amz_graph(state) {
       state.amzGraphVisible = !state.amzGraphVisible;
@@ -213,27 +76,45 @@ export default {
       state.currentItem.UrlImmagine = item.UrlImmagine;
       state.graph_params.EAN = item.EAN;
       state.graph_params.FiltroGiorni = "30";
+      console.log(item);
     },
     graph_request(state) {
       state.amzgraph_request.JsonRichiesta = JSON.stringify(state.graph_params);
     },
-    graph_success(state) {
-      state.status = "success";
+    // graph_success(state) {
+    //   state.status = "success";
+    // },
+    graph_success(state, { prcdata, labels, pdata, graph_data, max, min }) {
+      // state.prcdata = prcdata;
+      state.graph_data = graph_data;
+      state.chartdata.labels = labels;
+      state.chartdata.datasets[0] = pdata;
+      // state.options.scales.yAxes = scales;
+      // state.options.scales.xAxes[0].ticks.min = moment()
+      //   .subtract(29, "days")
+      //   .format();
+      // state.options.scales.xAxes[0].ticks.max = moment().format();
+      // scales.forEach((scale) => {
+      //   scale.ticks.suggestedMax = max;
+      //   scale.ticks.suggestedMin = min;
+      // });
+      // state.retailers = prcdata.ListaRetailers;
+      // state.listaRigheTabella = prcdata.ListaRigheTabella;
+      state.status = "Success";
+    },
+    prc_error(state, err) {
+      state.status = err;
+    },
+    clear_price(state) {
+      state.chartdata.labels = [];
+      state.chartdata.datasets = [];
     },
   },
   actions: {
-    save_graph({ commit }, item) {
-      commit("get_current_item", item);
-      commit("toggle_amz_graph");
-    },
-    amz_graph({ commit, state }, item) {
+    amz_graph({ commit, state }, { item }) {
       return new Promise((resolve, reject) => {
-        commit("get_current_item", item);
-        let graph_params = {
-          EAN: state.currentItem.EAN,
-          FiltroGiorni: "30",
-        };
-        commit("graph_request", graph_params);
+        commit("get_current_item", { item });
+        commit("graph_request");
         const richiesta = state.amzgraph_request;
         axios({
           url: "/",
@@ -244,7 +125,9 @@ export default {
           params: JSON.stringify(richiesta),
         })
           .then((res) => {
-            const current_item = JSON.parse(res.data.JsonRisposta);
+            console.log(res);
+
+            const graph_data = JSON.parse(res.data.JsonRisposta);
             let labels = [...new Array(30)].map((i, idx) =>
               moment
                 .utc()
@@ -253,19 +136,87 @@ export default {
                 .utcOffset(1)
                 .format()
             );
-            let chartdata = [];
-            current_item.ListaValori.forEach((item) => {
-              chartdata.push({});
+            labels.reverse();
+            let pdata = [];
+            let scales = [];
+
+            graph_data.ListaPrezzi.forEach((entry, idx, arr) => {
+              pdata.push({
+                // label: label.TestoLegenda,
+                // yAxisID: label.TestoLegenda,
+                type: "line",
+                data: arr.map((date) => {
+                  date.DataPrezzo = moment
+                    .utc(date.DataValore)
+                    .format("YYYY-MM-DD");
+
+                  return {
+                    x: date.DataPrezzo,
+                    y: date.PrezzoGiorno,
+                  };
+                }),
+
+                backgroundColor: "transparent",
+                borderWidth: 2,
+                borderColor: "#191919",
+                pointBorderWidth: 1,
+                pointRadius: 2,
+                pointBackgroundColor: "#191919",
+              });
+              // scales.push({
+              //   id: label.TestoLegenda,
+              //   position: idx == 0 ? "left" : "right",
+              //   ticks: {
+              //     source: "auto",
+              //     padding: 15,
+              //   },
+              //   gridLines: {
+              //     display: idx == 0 || idx == 1 ? true : false,
+              //     drawTicks: false,
+              //     offsetGridLines: false,
+              //     drawBorder: true,
+              //   },
+              // });
             });
-            commit("graph_success", current_item, labels);
+            // let max = 0;
+            // let min = null;
+            // let maxs = [];
+            // let lista_valori = [];
+            // pdata.forEach((data) => {
+            //   data.data.forEach((item) => {
+            //     max = item.y > max ? item.y : max;
+            //     maxs.push(max);
+            //     lista_valori.push(item.y);
+            //   });
+            // });
+            // max = Math.max.apply(null, maxs);
+            // min = Math.min.apply(null, lista_valori);
+            // console.log(min);
+
+            commit("graph_success", {
+              // graphdata,
+              labels,
+              pdata,
+              scales,
+              graph_data,
+              // max,
+              // min,
+            });
+
             resolve(res);
           })
-
           .catch((err) => {
+            commit("graph_error", err);
             reject(err);
           });
       });
     },
   },
-  getters: {},
+  getters: {
+    status: (state) => state.status,
+    pricedata: (state) => state.chartdata,
+    priceoptions: (state) => state.options,
+    listaRetailers: (state) => state.retailers,
+    listaRigheTabella: (state) => state.listaRigheTabella,
+  },
 };
--- src/store/modules/backup.js
+++ src/store/modules/backup.js
@@ -1,212 +1,273 @@
-.then((res) => {
-  //   let maxx = new Date();
-  //   let min_temp = new Date().setDate(maxx.getDate() - 30);
-  //   let min = new Date(min_temp);
-  //   min.toISOString();
-  //   const current_item = JSON.parse(res.data.JsonRisposta);
-  //   const total_days = current_item.ListaPrezzi;
-  //   total_days.reverse();
-  //   console.log(current_item);
-  //   let labels = [...new Array(31)].map((i, idx) =>
-  //     moment
-  //       .utc()
-  //       .startOf("day")
-  //       .subtract(idx, "days")
-  //       .utcOffset(1)
-  //       .format()
-  //   );
-  //   labels.reverse();
-  //   let prezzo_giorno = [];
-  //   let last_price = null;
-  //   let checked = [];
+import axios from "axios";
+import moment from "moment";
+import global from "@/store/modules/global.js";
 
-  //   for (const day in total_days) {
-  //     let data = total_days[day].DataPrezzo;
-  //     let data_finale = data.toString().slice(0, 10);
-  //     let next = parseInt(day) + 1;
-  //     let prev = parseInt(day) - 1;
-  //     let long = total_days.length;
-  //     if (day != long - 1) {
-  //       let data_due = total_days[next].DataPrezzo;
-  //       let data_finale_due = data_due.toString().slice(0, 10);
-  //       if (data_finale == data_finale_due) {
-  //         checked.push(total_days[day]);
-  //         if (total_days[prev] != undefined) {
-  //           let data_tre = total_days[prev].DataPrezzo;
-  //           let data_finale_tre = data_tre.toString().slice(0, 10);
-  //           if (data_finale == data_finale_tre) {
-  //             checked.push(total_days[day]);
-  //           }
-  //         }
-  //       }
-  //     } else {
-  //       break;
-  //     }
-  //   }
-  //   // for (const check of total_days){
-  //   //   let next = check.indexOf(check) + 1
-  //   //     // let stringa = total_days[datB].DataPrezzo;
-  //   //     // let date = moment(stringa).format()
-  //   //     // let lunghezza_x = labels[datA].length;
-  //   //     // let lunghezza_risposta = date.length
-  //   //     // let dataFinale = date.toString().slice(0, lunghezza_risposta -6)
-  //   //   if (total_days[check +1] != undefined){
-  //   //       if (total_days[check].DataPrezzo == total_days[check+ 1].DataPrezzo){
-  //   //         checked.push(total_days[check])
-  //   //       }
+export default {
+  state: {
+    amzGraphVisible: false,
+    graph_params: {},
+    graph_res: {},
+    graph_days: [],
+    currentItem: {},
 
-  //   //   }
-
-  //   //   else {break};
-
-  //   // }
-  //   let total_ranks = [];
-  //   let total_prices = [];
-  //   for (const rankA in total_days) {
-  //     let rank = total_days[rankA].SalesRankGiorno;
-  //     let price = total_days[rankA].PrezzoGiorno;
-  //     total_ranks.push(rank);
-  //     total_prices.push(price);
-  //   }
-  //   let max_price = Math.max.apply(null, total_prices);
-  //   let max_rank = Math.max.apply(null, total_ranks);
-  //   let max = Math.max(max_price, max_rank);
-  //   let sales_rank = [];
-  //   let last_rank = null;
-  //   let last_stock = null;
-  //   let in_stock_giorno = [];
-  //   for (const datA in labels) {
-  //     for (const datB in total_days) {
-  //       let stringa = total_days[datB].DataPrezzo;
-  //       var date = moment(stringa).format();
-  //       var dataControllo = moment(labels[datA]).format();
-
-  //       if (moment(date).isSame(dataControllo, "day")) {
-  //         if (total_days[datB].InStockGiorno == "No") {
-  //           last_stock = current_item.PrezzoMax;
-  //         } else {
-  //           last_stock = "";
-  //         }
-
-  //         last_rank = total_days[datB].SalesRankGiorno;
-  //         last_price = total_days[datB].PrezzoGiorno;
-  //       }
-  //     }
-  //     in_stock_giorno.push(last_stock);
-  //     prezzo_giorno.push(last_price);
-  //     sales_rank.push(last_rank);
-  //   }
-  //   in_stock_giorno.pop();
-  //   sales_rank.reverse();
-  //   let prezzoMax = Math.max.apply(Math, prezzo_giorno);
-  //   let prezzoMin = Math.min.apply(Math, prezzo_giorno);
-
-  //   for (const rankA in labels) {
-  //     for (const rankB in total_days) {
-  //       let stringa = total_days[rankB].DataPrezzo;
-  //       let date = moment(stringa).format();
-  //       let dataControllo = moment(labels[rankA]).format();
-  //       if (moment(date).isSame(dataControllo, "day")) {
-  //         // last_price = total_days[rankB].PrezzoGiorno;
-  //       }
-  //     }
-  //     // sales_rank.push(last_rank);
-  //   }
-
-  //   let temp_rank = sales_rank.reverse();
-  //   let min_rank = Math.min.apply(null, temp_rank);
-  //   let minimo = Math.min(prezzoMin, min_rank);
-  //   // for (const stock in labels) {
-  //   //   if (total_days[stock]) {
-  //   //     if (total_days[stock].InStockGiorno == "No") {
-  //   //       in_stock_giorno.push(max + 2);
-  //   //     } else {
-  //   //       in_stock_giorno.push("");
-  //   //     }
-  //   //   } else {
-  //   //     in_stock_giorno.push("");
-  //   //   }
-  //   // }
-
-  //   commit("graph_success", {
-  //     labels,
-  //     sales_rank,
-  //     prezzo_giorno,
-  //     in_stock_giorno,
-  //     current_item,
-  //     prezzoMax,
-  //     prezzoMin,
-  //     min_rank,
-  //     max_rank,
-  //     max,
-  //     minimo,
-  //     checked,
-  //     // maxx
-  //   });
-
-  //   resolve(res);
-  //   commit("toggle_amz_graph");
-  // })
-
-
-  clear_chart(state) {
-    state.chartdata = {
+    chartdata: {
       labels: [],
       datasets: [
         {
           label: "Prices",
-          backgroundColor: "transparent",
-          borderColor: "#409EFF",
-          borderWidth: 3,
-          borderSkipped: true,
-          data: [],
           type: "line",
-          pointBorderColor: "#409EFF",
-          pointBackgroundColor: "#fff",
-          pointBorderWidth: 1,
-          pointHoverRadius: 5,
-          pointHoverBackgroundColor: "#409EFF",
-          pointHoverBorderColor: "rgba(220,220,220,1)",
-          pointHoverBorderWidth: 2,
-          pointRadius: 3,
-          pointHitRadius: 10,
-          lineTension: 0,
-          order: 1,
           yAxisID: "prices",
-          steppedLine: "before",
         },
         {
           label: "Sales Rank",
-          backgroundColor: "transparent",
-          borderColor: "red",
-          borderWidth: 0.5,
-          data: [],
-          order: 2,
-          yAxisID: "ranks",
           type: "line",
-          lineTension: 0,
-          steppedLine: "before",
-          pointBorderColor: "red",
-          pointBackgroundColor: "#fff",
-          pointBorderWidth: 1,
-          pointHoverRadius: 5,
-          pointHoverBackgroundColor: "red",
-          pointHoverBorderColor: "rgba(220,220,220,1)",
-          pointHoverBorderWidth: 2,
-          pointRadius: 2,
-          pointHitRadius: 10,
+          yAxisID: "ranks",
         },
         {
           label: "Out of stock",
-          backgroundColor: "#ffb8ab",
-          borderColor: "red",
-          data: [],
+
           type: "bar",
-          order: 3,
+
           yAxisID: "stock",
-          categoryPercentage: 1.1,
-          barPercentage: 1.1,
         },
       ],
-    };
+    },
+    options: {
+      legend: {
+        labels: {
+          useLineStyle: true,
+          // eslint-disable-next-line no-unused-vars
+          filter: function (item, chart) {
+            // Logic to remove a particular legend item goes here
+            return !item.text.includes("Out of stock");
+          },
+        },
+      },
+      annotation: {
+        annotations: [
+          {
+            id: "min-line",
+            mode: "horizontal",
+            type: "line",
+            scaleID: "prices",
+            value: "",
+            borderColor: "rgba(255,0,0,0.4)",
+            borderWidth: 2,
+            borderDash: [2, 2],
+          },
+          {
+            id: "max-line",
+            mode: "horizontal",
+            type: "line",
+            scaleID: "prices",
+            value: "",
+            borderColor: "rgba(0,204,102,0.4)",
+            borderWidth: 2,
+            borderDash: [2, 2],
+          },
+        ],
+        drawTime: "afterDraw",
+      },
+      responsive: true,
+      maintainAspectRatio: false,
+      showLines: true,
+      scales: {
+        yAxes: [
+          {
+            id: "prices",
+            type: "linear",
+            position: "left",
+            gridLines: {
+              display: true,
+              offsetGridLines: false,
+            },
+            ticks: {
+              display: true,
+              // eslint-disable-next-line no-unused-vars
+              callback: function (value, index, values) {
+                return value + " " + "€";
+              },
+              beginAtZero: false,
+              // suggestedMax: "",
+              bounds: "data",
+              // min: "",
+              // max: "",
+              precision: 0,
+            },
+            afterTickToLabelConversion: function (scaleInstance) {
+              // set the first and last tick to null so it does not display
+              // note, ticks[0] is the last tick and ticks[length - 1] is the first
+              scaleInstance.ticks[scaleInstance.ticks.length - 1] = null;
+
+              // need to do the same thing for this similiar array which is used internally
+              scaleInstance.ticksAsNumbers[
+                scaleInstance.ticksAsNumbers.length - 1
+              ] = null;
+            },
+          },
+          {
+            id: "ranks",
+            type: "linear",
+            position: "right",
+            gridLines: {
+              display: false,
+              drawBorder: true,
+              lineWidth: 1,
+              zeroLineWidth: 1,
+              offsetGridLines: false,
+            },
+            ticks: {
+              display: false,
+              reverse: true,
+              precision: 0,
+              bounds: "data",
+            },
+          },
+          {
+            id: "stock",
+            distribution: "linear",
+            gridLines: {
+              display: false,
+            },
+            ticks: {
+              display: false,
+              precision: 0,
+              source: "data",
+              // min: "",
+              // max: ""
+            },
+          },
+        ],
+        xAxes: [
+          {
+            barPercentage: 1.1,
+            categoryPercentage: 1.1,
+
+            // bounds: "ticks",
+            // type: "time",
+            ticks: {
+              // min: "",
+              // max: "",
+              // source: "ticks",
+              userCallback: function (item, index) {
+                if (!(index % 3)) return moment(item).format("DD MMM");
+              },
+              autoSkip: true,
+              maxRotation: 0,
+              maxTicksLimit: 30,
+            },
+            distribution: "linear",
+            time: {
+              displayFormats: {
+                day: "D MMM",
+              },
+              // stepSize: 3,
+              unit: "day",
+            },
+            gridLines: {
+              display: true,
+              drawTicks: false,
+              offsetGridLines: false,
+              zeroLineWidth: 1,
+            },
+            offset: false,
+          },
+        ],
+      },
+      tooltips: {
+        mode: "index",
+        position: "nearest",
+        callbacks: {
+          title: (tooltipItems) => {
+            return tooltipItems[0].xLabel;
+          },
+
+          //
+          // eslint-disable-next-line no-unused-vars
+          label: function (t, d) {
+            if (t.datasetIndex === 0) {
+              return "Prezzo: " + t.yLabel + " " + "€";
+            } else if (t.datasetIndex === 1) {
+              return "Sales Rank: " + t.yLabel;
+            }
+          },
+        },
+      },
+    },
+    days: [],
+    min_max: [],
+    amzgraph_request: {
+      CodiceClient: "reevolacerba2020",
+      CodiceRichiesta: "AMZGraph",
+      VersioneClient: global.state.VersioneClient,
+      Url: window.location.href,
+      //define JsonRichiesta from mutations;
+    },
   },
+
+  mutations: {
+    toggle_amz_graph(state) {
+      state.amzGraphVisible = !state.amzGraphVisible;
+    },
+    get_current_item(state, { item }) {
+      state.currentItem.UrlImmagine = item.UrlImmagine;
+      state.graph_params.EAN = item.EAN;
+      state.graph_params.FiltroGiorni = "30";
+    },
+    graph_request(state) {
+      state.amzgraph_request.JsonRichiesta = JSON.stringify(state.graph_params);
+    },
+    graph_success(state) {
+      state.status = "success";
+    },
+  },
+  actions: {
+    save_graph({ commit }, item) {
+      commit("get_current_item", item);
+      commit("toggle_amz_graph");
+    },
+    amz_graph({ commit, state }, item) {
+      return new Promise((resolve, reject) => {
+        commit("get_current_item", item);
+        let graph_params = {
+          EAN: state.currentItem.EAN,
+          FiltroGiorni: "30",
+        };
+        commit("graph_request", graph_params);
+        const richiesta = state.amzgraph_request;
+        axios({
+          url: "/",
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          params: JSON.stringify(richiesta),
+        })
+          .then((res) => {
+            const current_item = JSON.parse(res.data.JsonRisposta);
+            let labels = [...new Array(30)].map((i, idx) =>
+              moment
+                .utc()
+                .startOf("day")
+                .subtract(idx, "days")
+                .utcOffset(1)
+                .format()
+            );
+            let chartdata = [];
+            current_item.ListaValori.forEach((item, idx) => {
+              chartdata.push({
+                label: idx == 0 ? ''
+              });
+            });
+            commit("graph_success", current_item, labels);
+            resolve(res);
+          })
+
+          .catch((err) => {
+            reject(err);
+          });
+      });
+    },
+  },
+  getters: {},
+};
