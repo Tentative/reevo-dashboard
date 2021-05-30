1424d4e5ad11efdf8593fe40ba7194c4477e10d0
Tentative <micoli.giacomo@gmail.com>
2021-05-30 21:01:44 +0200

    fix: discordant pagination in items

--- src/views/Items.vue
+++ src/views/Items.vue
@@ -9,20 +9,26 @@
     </md-dialog>
     <Table :loaded="loaded" />
     <div v-if="loaded" class="pagination-nav">
-      <Pagination />
+      <Pagination v-model="pagination" @change-page="onChangePage($event)" />
       <Jumper />
     </div>
   </div>
 </template>
 
 <script>
-import { Table, Pagination, Jumper, spinner } from "@/components/Items/";
+import { Table, Jumper, spinner } from "@/components/Items/";
+import { Pagination } from "@/components/AMZ";
 import { mapGetters } from "vuex";
 export default {
   name: "Items",
   components: { Table, Pagination, Jumper, spinner },
   data() {
     return {
+      pagination: {
+        currentPage: 1,
+        itemsPerPage: 20,
+        totalPages: "",
+      },
       loading: false,
       loaded: false,
     };
@@ -38,19 +44,29 @@ export default {
   },
 
   methods: {
-    items_request() {
+    onChangePage(v) {
+      this.pagination = v;
+      this.items_request();
+    },
+    async items_request() {
       this.loading = true;
       let itm = this.itm;
-      this.$store
+      itm.ItemsPerPagina = this.pagination.itemsPerPage;
+      itm.NumeroPagina = this.pagination.currentPage;
+      await this.$store
         .dispatch("itm_request", { itm })
-
         // eslint-disable-next-line no-unused-vars
         .then((res) => {
+          console.log(res, "res");
+          let params = JSON.parse(res.data.JsonRisposta);
+          this.pagination.currentPage = params.NumeroPagina;
+          this.pagination.totalPages = params.QtaPagine;
+          this.pagination.itemsPerPage = params.ItemsPerPagina;
           // console.log("check su itm" + { itm });
           // console.log(res);
           this.loading = false;
           this.loaded = true;
-          this.items = this.$store.getters.itm;
+          this.items = params;
         });
       // .catch(err => console.log(err));
     },
